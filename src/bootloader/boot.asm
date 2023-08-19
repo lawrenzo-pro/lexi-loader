22b836322ba41ef17c4d42abc68da3868f1b6239
Lawrence Kimani <larrykimani01@gmail.com>
2023-08-19 21:04:15 +0300

    Improved fat support

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -2,145 +2,268 @@ org 0x7C00
 bits 16
 
 %define ENDL 0x0D, 0x0A
-
-;magic stuff
 jmp short start
 nop
-;more magic
-bdb_oem:    db 'MSWIN4.1' 
-bdb_bytes_per_sector:   dw 512
-bdb_sectors_per_cluster: db 1
-bdb_reserved_sectors:   dw 1
-bdb_fat_count: db 2
-bdb_dir_entries_count: dw 0E0h
-bdb_total_sectors:  dw 2880
-bdb_media_descriptor_type: db 0F0h
-bdb_sectors_per_fat:    dw 9
-bdb_sectors_per_track:  dw 18
-bdb_heads: dw 2
-bdb_hidden_sectors: dd 0
-bdb_large_sector_count:     dd 0
-;the magic is almost ending
-ebr_drive_number: db 0
-                  db 0
-ebr_signature: db 29h
-ebr_volume_id: db 12h, 34h, 56h, 78h 
-ebr_volume_label: db 'LEXI   '
-ebr_system_id: db 'FAT12   '
 
-;end magic
+bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
+bdb_bytes_per_sector:       dw 512
+bdb_sectors_per_cluster:    db 1
+bdb_reserved_sectors:       dw 1
+bdb_fat_count:              db 2
+bdb_dir_entries_count:      dw 0E0h
+bdb_total_sectors:          dw 2880                 
+bdb_media_descriptor_type:  db 0F0h                 
+bdb_sectors_per_fat:        dw 9                    
+bdb_sectors_per_track:      dw 18
+bdb_heads:                  dw 2
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
+ebr_drive_number:           db 0                    
+                            db 0                    
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   
+ebr_volume_label:           db 'LEXI OS'        
+ebr_system_id:              db 'FAT12   '           
 start:
-    jmp main
-;ds:si points to string
+    mov ax, 0           
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00          
+    push es
+    push word .after
+    retf
+
+.after:
+    mov [ebr_drive_number], dl
+
+    ; show loading message
+    mov si, msg_loading
+    call puts
+
+    push es
+    mov ah, 08h
+    int 13h
+    jc floppy_error
+    pop es
+
+    and cl, 0x3F                        ; remove top 2 bits
+    xor ch, ch
+    mov [bdb_sectors_per_track], cx     ; sector count
+
+    inc dh
+    mov [bdb_heads], dh                 ; head count
+
+    mov ax, [bdb_sectors_per_fat]
+    mov bl, [bdb_fat_count]
+    xor bh, bh
+    mul bx                             
+    add ax, [bdb_reserved_sectors]      
+    push ax
+
+    mov ax, [bdb_dir_entries_count]
+    shl ax, 5                           
+    xor dx, dx                          
+    div word [bdb_bytes_per_sector]     
+
+    test dx, dx                         
+    jz .root_dir_after
+    inc ax                              
+.root_dir_after:
+
+    ; read root directory
+    mov cl, al                          
+    pop ax                              
+    mov dl, [ebr_drive_number]          
+    mov bx, buffer                   
+    call disk_read
+    ; search for kernel.bin
+    xor bx, bx
+    mov di, buffer
+
+.search_kernel:
+    mov si, file_kernel_bin
+    mov cx, 11                          ; compare up to 11 characters
+    push di
+    repe cmpsb
+    pop di
+    je .found_kernel
+
+    add di, 32
+    inc bx
+    cmp bx, [bdb_dir_entries_count]
+    jl .search_kernel
+
+    ; kernel not found
+    jmp kernel_not_found_error
+
+.found_kernel:
+    mov ax, [di + 26]                   
+    mov [kernel_cluster], ax
+
+    ; load FAT from disk into memory
+    mov ax, [bdb_reserved_sectors]
+    mov bx, buffer
+    mov cl, [bdb_sectors_per_fat]
+    mov dl, [ebr_drive_number]
+    call disk_read
+
+    ; read kernel and process FAT chain
+    mov bx, KERNEL_LOAD_SEGMENT
+    mov es, bx
+    mov bx, KERNEL_LOAD_OFFSET
+
+.load_kernel_loop:
+    
+    mov ax, [kernel_cluster]
+    
+    add ax, 31                           
+    mov cl, 1
+    mov dl, [ebr_drive_number]
+    call disk_read
+
+    add bx, [bdb_bytes_per_sector]
+
+    ; compute location of next cluster
+    mov ax, [kernel_cluster]
+    mov cx, 3
+    mul cx
+    mov cx, 2
+    div cx                              
+
+    mov si, buffer
+    add si, ax
+    mov ax, [ds:si]                     
+
+    or dx, dx
+    jz .even
+
+.odd:
+    shr ax, 4
+    jmp .next_cluster_after
+
+.even:
+    and ax, 0x0FFF
+
+.next_cluster_after:
+    cmp ax, 0x0FF8                      
+    jae .read_finish
+
+    mov [kernel_cluster], ax
+    jmp .load_kernel_loop
+
+.read_finish:
+    
+    ; jump to our kernel
+    mov dl, [ebr_drive_number]          
+
+    mov ax, KERNEL_LOAD_SEGMENT         
+    mov ds, ax
+    mov es, ax
+
+    jmp KERNEL_LOAD_SEGMENT:KERNEL_LOAD_OFFSET
+
+    jmp wait_key_and_reboot             ; should never happen
+
+    cli                                 
+    hlt
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+kernel_not_found_error:
+    mov si, msg_kernel_not_found
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h                     
+    jmp 0FFFFh:0                
+
+.halt:
+    cli                         
+    hlt
+
 puts:
+    ; save registers we will modify
     push si
     push ax
     push bx
+
 .loop:
-    lodsb
-    or al, al
-
+    lodsb               
+    or al, al           
     jz .done
-    mov ah, 0x0E
 
-    mov bh, 0
+    mov ah, 0x0E        
+    mov bh, 0           
     int 0x10
+
     jmp .loop
 
 .done:
     pop bx
     pop ax
-    pop si
+    pop si    
     ret
-main:
-   ; setup data segments
-   mov ax, 0
-   mov ds, ax
-   mov es, ax
-
-   ; setup stack
-   mov ss, ax
-   mov sp, 0x7C00
-   ;magic
-   mov [ebr_drive_number], dl
-   mov ax, 1
-   mov cl, 1
-   mov bx, 0x7E00
-   call disk_read
-
-   ;more magic
-   mov si, message
-   call puts
-
-   ;halt
-   cli ;ignore all interrupts
-   hlt
-;Messed up? what's next?
-floppy_error:
-    mov si, msg_read_failed
-    call puts
-    jmp wait_key_and_reboot
-wait_key_and_reboot:
-    mov ah, 0
-    int 16h
-    jmp 0FFFFh:0
-;the computer shouldn't get here, but if it does let's make it stop
-.halt:
-    cli
-    hlt
-
-;magic routines
 lba_to_chs:
+
     push ax
     push dx
 
-    xor dx,dx
-    div word [bdb_sectors_per_track]
+    xor dx, dx                          
+    div word [bdb_sectors_per_track]    
+                                        
 
-    inc cx
-    mov cx,dx
+    inc dx                              
+    mov cx, dx                        
 
-    xor dx,dx
-    div word [bdb_heads]
+    xor dx, dx                          
+    div word [bdb_heads]                
 
-    mov dh,dl
-    mov ch,al
-    shl ah,6
-    or cl,ah
+    mov dh, dl                         
+    mov ch, al                          
+    shl ah, 6
+    or cl, ah                           
 
     pop ax
-    mov dl,al
+    mov dl, al                          
     pop ax
     ret
+
 disk_read:
-    ;lets push stuff
-    push ax
+
+    push ax                             ; save registers we will modify
     push bx
     push cx
     push dx
     push di
 
-    push cx
-    call lba_to_chs
-    pop ax
-
+    push cx                             
+    call lba_to_chs                    
+    pop ax                              
+    
     mov ah, 02h
-    mov di, 3
-.retry:
-    pusha
-    stc
-    int 13h
-    jnc .done
+    mov di, 3                           
 
-    ;we failed? What's next?
+.retry:
+    pusha                              
+    stc                                 
+    int 13h                             
+    jnc .done                          
+    ; read failed
     popa
-    call dish_reset
+    call disk_reset
 
     dec di
-    test di,di
+    test di, di
     jnz .retry
+
 .fail:
+    ; all attempts are exhausted
     jmp floppy_error
+
 .done:
     popa
 
@@ -148,19 +271,30 @@ disk_read:
     pop dx
     pop cx
     pop bx
-    pop ax
+    pop ax                             
     ret
-dish_reset:
+
+disk_reset:
     pusha
-    mov ah,0
+    mov ah, 0
     stc
     int 13h
     jc floppy_error
     popa
     ret
-;strings
-message: db 'Hi There !',ENDL ,0
-msg_read_failed: db 'An error occured when trying to read the disk!', ENDL, 0
-;the magic ends
+
+
+msg_loading:            db 'Loading...', ENDL, 0
+msg_read_failed:        db 'Read from disk failed!', ENDL, 0
+msg_kernel_not_found:   db 'KERNEL.BIN file not found!', ENDL, 0
+file_kernel_bin:        db 'KERNEL  BIN'
+kernel_cluster:         dw 0
+
+KERNEL_LOAD_SEGMENT     equ 0x2000
+KERNEL_LOAD_OFFSET      equ 0
+
+
 times 510-($-$$) db 0
 dw 0AA55h
+
+buffer:
--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,47 +1,34 @@
-org 0x7C00
+org 0x0
 bits 16
 
 %define ENDL 0x0D, 0x0A
 
 start:
-    jmp main
-;ds:si points to string
+    mov si, message
+    call puts
+.halt:
+    cli
+    hlt
 puts:
+    ; save registers we will modify
     push si
     push ax
+    push bx
 
 .loop:
-    lodsb
-    or al, al
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
     jz .done
-    mov ah, 0x0e
-    mov bh, 0
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
     int 0x10
+
     jmp .loop
 
 .done:
+    pop bx
     pop ax
-    pop si
+    pop si    
     ret
-main:
-   ; setup data segments
-   mov ax, 0
-   mov ds, ax
-   mov es, ax
-
-   ; setup stack
-   mov ss, ax
-   mov sp, 0x7C00
-
-   ;magic
-   mov si, message
-   call puts
-
-   ;halt
-   hlt
-
-.halt:
-    jmp .halt
-message: db 'Hi There !',ENDL ,0
-times 510-($-$$) db 0
-dw 0AA55h
+message: db 'Hello world from KERNEL!', ENDL, 0
